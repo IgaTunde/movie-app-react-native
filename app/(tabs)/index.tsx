9c027d16c69df30e9d6cc80d2fd837ac06be3b08
Iga Tunde <igatunde06@gmail.com>
2025-07-02 17:22:58 +0100

    splash

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { StatusBar, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="bg-light-200 flex-1 justify-center items-center">
+    <View className=" flex-1 justify-center items-center">
       <StatusBar barStyle="dark-content" />
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
     </View>
