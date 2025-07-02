69ea50ce9b3b38e5cf0fda435e9815e181aea293
Iga Tunde <igatunde06@gmail.com>
2025-07-02 11:40:05 +0100

    movie app

---
+++ app/movie/[id].tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>
+        MovieDetails Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Magnam, delectus ipsa quaerat mollitia culpa vero ab voluptatibus odit
+        quae dolorum fugit, architecto natus maiores explicabo deleniti minus
+        magni! Ex, sed.
+      </Text>
+    </View>
+  );
+};
+
+export default MovieDetails;
