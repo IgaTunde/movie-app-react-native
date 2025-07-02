dcfed114c5c077a08caf69f13a66962542f1bbc5
Iga Tunde <igatunde06@gmail.com>
2025-07-02 18:21:16 +0100

    api in progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,30 @@
-import { StatusBar, Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, StatusBar, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className=" flex-1 justify-center items-center">
-      <StatusBar barStyle="dark-content" />
-      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+    <View className=" flex-1 bg-primary">
+      <StatusBar barStyle="light-content" />
+      <Image source={images.bg} className="absolute w-full" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className=" w-12 h-10 mt-20 mx-auto mb-5" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
